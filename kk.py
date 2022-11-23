e5249a88e56455e864f531dfe408f6ad980e34ae
Lluís Pellejà <lluispeso@gmail.com>
2022-11-23 09:50:44 +0100

    Ya esta acabado

--- kk.py
+++ kk.py
@@ -1,1 +1,19 @@
 print("Hello World!")
+
+
+def FizzBuzz(n):
+    res = []
+
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            res.append('FizzBuzz')
+        elif i % 3 == 0:
+            res.append('Fizz')
+        elif i % 5 == 0:
+            res.append('Buzz')
+        else:
+            res.append(i)
+    
+    return res
+
+
